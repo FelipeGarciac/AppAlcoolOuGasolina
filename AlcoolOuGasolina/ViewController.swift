51073f603195701bde0c74c69fa1e809edba27b3
felipe <garcia.felipe180180@gmail.com>
2023-05-25 20:14:26 -0300

    Initial Commit

---
+++ AlcoolOuGasolina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlcoolOuGasolina
+//
+//  Created by Felipe Garcia on 25/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
